9690678fbabff570f37967d8c467d38966c6e609
FaithDaka <faithbusigu8@gmail.com>
2023-03-14 13:54:49 +0300

    colors

--- platform/tailwind.config.js
+++ platform/tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      gridTemplateColumns: {
+        pageWidth: '1fr minmax(0, 1088px) 1fr',
+      },
       fontFamily: {
         sans: ['Inter', ...defaultTheme.fontFamily.sans],
       },
@@ -23,17 +26,25 @@ module.exports = {
         'baby-blue': '#F5F8FF',
         grey: {
           100: '#363A4414',
-          150: '#363A4429',
+          150: '#0000000F',
           300: '#6D7175',
-          150: '#363A4429',
-          100: '#363A4414',
+          // 150: '#363A4429',
+          // 100: '#363A4414',
         },
-        skeleton: '#363A4429',
+        skeleton: '#F9F9F9',
         black: {
           600: '#202223',
         },
+        // which black is which?
         black: {
           600: '#353E52',
+        },
+        green: {
+          50: '#C6FFE4',
+          550: '#0CE87E',
+        },
+        orange: {
+          450: '#FE9E35',
         },
       },
     },
