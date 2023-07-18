bb3750af08fa30466b7edb831a2b4feb84c44e79
FaithDaka <faithbusigu8@gmail.com>
2023-07-18 09:35:55 +0300

    implement redirect

--- platform/src/core/apis/Account.js
+++ platform/src/core/apis/Account.js
@@ -5,5 +5,13 @@ export const postUserCreationDetails = async (data) =>
   await axios.post(AUTH_URL, data).then((response) => response.data);
 
 export const getGoogleAuthDetails = async () => {
-  await axios.get(GOOGLE_AUTH_URL);
+  try {
+    const response = await axios.get(GOOGLE_AUTH_URL);
+    if (response.status === 302) {
+      const redirectUrl = response.headers.location;
+      window.location.href = redirectUrl;
+    }
+  } catch (error) {
+    alert(error)
+  }
 };
